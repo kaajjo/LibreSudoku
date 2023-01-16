5d4be2a72b6f186accdbb7acaefb9933d85ac9a9
kaajjo <claymanoff@gmail.com>
2023-01-16 19:31:45 +0300

    About screen: Add help translate and app logo

--- app/src/main/java/com/kaajjo/libresudoku/ui/more/about/AboutScreen.kt
+++ app/src/main/java/com/kaajjo/libresudoku/ui/more/about/AboutScreen.kt
@@ -1,14 +1,19 @@
 package com.kaajjo.libresudoku.ui.more.about
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.kaajjo.libresudoku.BuildConfig
 import com.kaajjo.libresudoku.R
 import com.kaajjo.libresudoku.ui.components.PreferenceRow
@@ -39,11 +44,32 @@ fun AboutScreen(
                 .padding(it)
                 .fillMaxWidth()
         ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(56.dp),
+                    painter = painterResource(R.drawable.ic_launcher_foreground),
+                    contentDescription = null,
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+
+            Divider()
+
             PreferenceRow(
                 title = stringResource(R.string.about_version),
                 subtitle = BuildConfig.VERSION_NAME,
                 painter = painterResource(R.drawable.ic_outline_info_24),
             )
+
             val uriHandler = LocalUriHandler.current
             PreferenceRow(
                 title = stringResource(R.string.about_github_project),
@@ -52,6 +78,15 @@ fun AboutScreen(
                     uriHandler.openUri("https://github.com/kaajjo/Libre-Sudoku")
                 }
             )
+
+            PreferenceRow(
+                title = stringResource(R.string.help_translate),
+                painter = painterResource(R.drawable.round_translate_24),
+                onClick = {
+                    uriHandler.openUri("https://hosted.weblate.org/engage/libresudoku/")
+                }
+            )
+
             PreferenceRow(
                 title = stringResource(R.string.libraries_licenses_title),
                 painter = painterResource(R.drawable.ic_outline_info_24),
