43e03e28c139d6868e09bba76a023aef3a6cb365
kaajjo <claymanoff@gmail.com>
2023-04-29 08:03:17 +0300

    Add `systemBarsPadding` to the `WelcomeScreen`

--- app/src/main/java/com/kaajjo/libresudoku/ui/onboarding/WelcomeScreen.kt
+++ app/src/main/java/com/kaajjo/libresudoku/ui/onboarding/WelcomeScreen.kt
@@ -1,6 +1,11 @@
 package com.kaajjo.libresudoku.ui.onboarding
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.FilledTonalButton
@@ -37,7 +42,7 @@ fun WelcomeScreen(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .statusBarsPadding(),
+            .systemBarsPadding(),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Column(
