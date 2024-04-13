1d9330cd5421c98f327788021b40efbe97d066d4
kaajjo <claymanoff@gmail.com>
2024-04-13 15:03:54 +0300

    chore: change `versionName` to `2.0.0-beta1`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 7
-        versionName = "1.5.0"
+        versionName = "2.0.0-beta1"
 
         vectorDrawables {
             useSupportLibrary = true
