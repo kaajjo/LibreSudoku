39d6528970f106fba3a20c7499c7ad32bf6bc1ef
nvllz <110673332+nvllz@users.noreply.github.com>
2024-04-28 15:34:39 +0000

    Update SettingsBackup.kt
    Fixed a typo.

--- app/src/main/java/com/kaajjo/libresudoku/data/backup/SettingsBackup.kt
+++ app/src/main/java/com/kaajjo/libresudoku/data/backup/SettingsBackup.kt
@@ -26,7 +26,7 @@ data class SettingsBackup(
     val funKeyboardOverNum: Boolean = PreferencesConstants.DEFAULT_FUN_KEYBOARD_OVER_NUM,
     val dateFormat: String = "",
     val saveSelectedGameDifficulty: Boolean = PreferencesConstants.DEFAULT_SAVE_LAST_SELECTED_DIFF_TYPE,
-    val autoBackupInteral: Long = PreferencesConstants.DEFAULT_AUTOBACKUP_INTERVAL,
+    val autoBackupInterval: Long = PreferencesConstants.DEFAULT_AUTOBACKUP_INTERVAL,
     val maxAutoBackups: Int = PreferencesConstants.DEFAULT_AUTO_BACKUPS_NUMBER,
     val dynamicColors: Boolean = PreferencesConstants.DEFAULT_DYNAMIC_COLORS,
     val darkTheme: Int = PreferencesConstants.DEFAULT_DARK_THEME,
