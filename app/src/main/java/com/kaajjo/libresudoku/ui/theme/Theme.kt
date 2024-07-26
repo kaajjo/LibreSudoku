fa54dc649c72e7fa7acc790bbc92525654134c9f
kaajjo <claymanoff@gmail.com>
2024-07-26 15:54:54 +0300

    ui: animate colorScheme change * dep: update `materialKolor`

--- app/src/main/java/com/kaajjo/libresudoku/ui/theme/Theme.kt
+++ app/src/main/java/com/kaajjo/libresudoku/ui/theme/Theme.kt
@@ -1,8 +1,9 @@
 package com.kaajjo.libresudoku.ui.theme
 
 import android.os.Build
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
@@ -10,8 +11,9 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.materialkolor.DynamicMaterialTheme
 import com.materialkolor.PaletteStyle
-import com.materialkolor.rememberDynamicColorScheme
+import com.materialkolor.rememberDynamicMaterialThemeState
 
 @Composable
 fun LibreSudokuTheme(
@@ -23,36 +25,44 @@ fun LibreSudokuTheme(
     paletteStyle: PaletteStyle = PaletteStyle.TonalSpot,
     content: @Composable () -> Unit,
 ) {
-    
-    val currentTheme = rememberDynamicColorScheme(
+    var materialThemeState = rememberDynamicMaterialThemeState(
         seedColor = colorSeed,
         isDark = darkTheme,
-        style = paletteStyle
+        style = paletteStyle,
+        isAmoled = amoled
     )
 
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
+    var colorScheme = materialThemeState.colorScheme
+    if (dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val context = LocalContext.current
 
-            when {
-                darkTheme && amoled -> dynamicDarkColorScheme(context).copy(
-                    background = Color.Black,
-                    surface = Color.Black
-                )
+        colorScheme = when {
+            darkTheme && amoled -> dynamicDarkColorScheme(context).copy(
+                background = Color.Black,
+                surface = Color.Black
+            )
 
-                darkTheme && !amoled -> dynamicDarkColorScheme(context)
-                else -> dynamicLightColorScheme(context)
-            }
+            darkTheme && !amoled -> dynamicDarkColorScheme(context)
+            else -> dynamicLightColorScheme(context)
         }
-
-        darkTheme && amoled -> currentTheme.copy(background = Color.Black, surface = Color.Black)
-        darkTheme -> currentTheme
-        else -> currentTheme
     }
     val systemUiController = rememberSystemUiController()
 
-    MaterialTheme(
-        colorScheme = colorScheme,
+    materialThemeState = rememberDynamicMaterialThemeState(
+        seedColor = colorSeed,
+        isDark = darkTheme,
+        style = paletteStyle,
+        isAmoled = amoled,
+        modifyColorScheme = { colorScheme }
+    )
+
+    DynamicMaterialTheme(
+        state = materialThemeState,
+        animate = true,
+        animationSpec = tween(
+            durationMillis = 200,
+            easing = FastOutSlowInEasing
+        ),
         typography = Typography,
         content = {
             SideEffect {
