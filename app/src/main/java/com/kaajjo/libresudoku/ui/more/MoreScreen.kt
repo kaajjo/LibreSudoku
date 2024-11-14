453d54f7deb4282ad40d46e64b575861fa67e170
kaajjo <claymanoff@gmail.com>
2024-11-14 20:33:21 +0300

    fix(MoreScreen): apply window insets

--- app/src/main/java/com/kaajjo/libresudoku/ui/more/MoreScreen.kt
+++ app/src/main/java/com/kaajjo/libresudoku/ui/more/MoreScreen.kt
@@ -18,11 +18,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -87,7 +89,9 @@ fun MoreScreen(
     val autoUpdateChannel by viewModel.updateChannel.collectAsStateWithLifecycle(UpdateChannel.Disabled)
     val updateDismissedName by viewModel.updateDismissedName.collectAsStateWithLifecycle("")
 
-    Scaffold { innerPadding ->
+    Scaffold(
+        contentWindowInsets = WindowInsets.statusBars
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxWidth()
